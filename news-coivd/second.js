70f02d4040ef6515f41d9a5b3d9401fa350b8d51
Hadi <85053006+Hadi2x@users.noreply.github.com>
2023-02-17 19:50:56 +0300

    Add files via upload

---
+++ news-coivd/second.js
@@ -0,0 +1,37 @@
+fetch("https://covid-19-statistics.p.rapidapi.com/reports/total?date=2020-04-07", {
+
+    "method": "GET",
+
+    "headers": {
+
+        "x-rapidapi-key": "2d2dea647fmsh577c57bb54abd69p13249djsn06827c6f1fe8",
+
+        "x-rapidapi-host": "covid-19-statistics.p.rapidapi.com"
+
+    }
+
+})
+
+.then(response => response.json())
+
+.then(data => {
+
+    var numbers = document.querySelectorAll('.numbers');
+
+ var num = new Intl.NumberFormat('en-US');
+
+    numbers[0].innerHTML = num.format(data.data.confirmed);
+
+    numbers[1].innerHTML = num.format(data.data.active);
+
+    numbers[2].innerHTML = num.format(data.data.deaths);
+
+    numbers[3].innerHTML = num.format(data.data.recovered);
+
+})
+
+.catch(err => {
+
+    console.error(err);
+
+});
